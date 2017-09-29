0e0fe3a5bd0c713783a5f7fe24731cca7ea1ca49
Thomas Baetens <tbaetens@cri.be>
2017-09-29 13:27:38 +0200

    DFS scripts

---
+++ check_dfs_backlog.ps1
@@ -0,0 +1,4 @@
+﻿$groupname = Read-Host "Please enter the DFS group"
+$srccomputer = Read-Host "Please enter the source DFS server"
+$destcomputer = Read-Host "Please enter the receiving DFS server"
+Get-DfsrBacklog -GroupName $groupname -FolderName * -SourceComputerName $srccomputer -DestinationComputerName $destcomputer -Verbose
---
+++ check_dfs_state.ps1
@@ -0,0 +1,2 @@
+﻿$computer = Read-Host "Please enter the receiving DFS server"
+get-dfsrstate -ComputerName $computer | sort UpdateState -Descending | ft path,inbound,updatestate,sourcecomputername -auto -wrap
