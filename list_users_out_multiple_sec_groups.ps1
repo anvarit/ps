9e376f7fdb2d7285017f2e254ac2ee865d1dc42f
Thomas Baetens <thomas@anvarit.com>
2019-11-22 15:21:35 +0100

    Create list_users_out_multiple_sec_groups.ps1

---
+++ list_users_out_multiple_sec_groups.ps1
@@ -0,0 +1,7 @@
+﻿$Grps = Get-ADGroup -Filter{name -like "BarcSafe*"}
+
+$results=Foreach ($Grp in $Grps){
+  Get-ADPrincipalGroupMembership -Identity $Grp.SamAccountName |
+   Get-ADGroupMember
+}
+$results | Export-csv 'c:\temp\secgrp.csv' -notypeinformation
